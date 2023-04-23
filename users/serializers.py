4a6ddf043fa2b851a87fee9cb3caae01d3672882
iarley1 <iarley96@gmail.com>
2023-04-23 17:49:40 -0300

    feat: task 1 finished

--- users/serializers.py
+++ users/serializers.py
@@ -1,33 +1,41 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 from .models import User
+from django.contrib.auth.hashers import make_password
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(
-        validators=[
-            UniqueValidator(
-                queryset=User.objects.all(),
-                message="A user with that username already exists.",
-            )
-        ],
-    )
-    email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
-    )
-    password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    is_superuser = serializers.BooleanField(read_only=True)
+class UserSerializer(serializers.ModelSerializer):
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This field must be unique.")
+        return value
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_superuser(**validated_data)
 
     def update(self, instance: User, validated_data: dict) -> User:
+        if validated_data["password"]:
+            validated_data["password"] = make_password(validated_data['password'])
         for key, value in validated_data.items():
             setattr(instance, key, value)
 
         instance.save()
 
         return instance
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "is_superuser",
+        ]
+
+        extra_kwargs = {
+            "is_superuser": {"read_only": True},
+            'password': {"write_only": True},
+            'email': {'validators': []},
+        }
